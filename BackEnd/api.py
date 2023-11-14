8c1b9c946c820f4eabe0df871ad6ca7e316cb4b1
John <121889006+jgw0915@users.noreply.github.com>
2023-11-14 11:30:09 +0000

    feat: get_response API

--- BackEnd/api.py
+++ BackEnd/api.py
@@ -1,1 +1,23 @@
-import 
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.getenv("openAI_key"),
+)
+
+
+def get_response(message):
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": message
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+
+    return chat_completion.choices[0].message.content
