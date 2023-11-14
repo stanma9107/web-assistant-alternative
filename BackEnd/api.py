53562106e49f62e7f3cf8f65004848a33b58f772
Ｌｅｏ <102800624+Leowwd@users.noreply.github.com>
2023-11-14 06:58:24 +0000

    feat:add api

--- BackEnd/api.py
+++ BackEnd/api.py
@@ -1,1 +1,16 @@
-import 
+from openai import OpenAI
+
+client = OpenAI(
+    # defaults to os.environ.get("OPENAI_API_KEY")
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Say this is a test",
+        }
+    ],
+    model="gpt-3.5-turbo",
+)
