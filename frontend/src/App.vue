8602c8b4d0f0c2688e969e3ba67f7528b9669b4b
yusyuanshih <a8005671900@gmail.com>
2023-11-21 14:15:03 +0800

    style: RWD

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,9 @@
 <template>
 <div class="w-full flex flex-col items-center justify-center min-h-screen">
-  <div class="w-[60%] flex-1 h-full">
+  <div class="w-[95%] lg:w-[60%] flex-1 h-full">
     <div class="chat chat-start">
 
-    <div class="chat-header">
+    <div class="chat-header text-base lg:text-xl">
       Obi-Wan Kenobi
       <time class="text-xs opacity-50">12:45</time>
     </div>
@@ -25,10 +25,12 @@
     </div>
     </div>
     </div>
-    <div class="w-[60%] my-2 join"> 
+    <div class="w-[95%] lg:w-[60%] my-2 join"> 
       <input type="text" placeholder="Type here" class="input input-bordered w-full join-item" />
       <button class="btn btn-square join-item">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
+        <img
+          src="/location-svgrepo-com.svg"
+        />
       </button>
     </div>
   </div>
