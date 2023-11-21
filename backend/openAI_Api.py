82ffc2038cdf877d349c676fad570dd298e95422
John <121889006+jgw0915@users.noreply.github.com>
2023-11-21 07:11:18 +0000

    feat : ght 3.5 turbo-1106 json format response

--- backend/openAI_Api.py
+++ backend/openAI_Api.py
@@ -1,23 +1,26 @@
 from openai import OpenAI
-import os
-from dotenv import load_dotenv
+import ujson
 
-load_dotenv()
-
-client = OpenAI(
-    api_key=os.getenv("openAI_key"),
-)
+client = OpenAI()
 
 
 def get_response(message):
     chat_completion = client.chat.completions.create(
-        messages=[
+        model="gpt-3.5-turbo-1106",
+        response_format={ "type": "json_object" },
+        messages= [
+            {
+             "role": "system", 
+             "content": "You are a helpful assistant designed to output JSON.\
+                        And your json format should only be { \" response \" = your answer }"
+             },  
             {
                 "role": "user",
                 "content": message
             }
-        ],
-        model="gpt-3.5-turbo",
+        ]
     )
 
-    return chat_completion.choices[0].message.content
+    json_response = ujson.loads(chat_completion.choices[0].message.content)
+
+    return json_response
