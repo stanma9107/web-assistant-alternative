75e9c09b50b04cf5b19bdf408265b6ad83d90c67
Ｌｅｏ <102800624+Leowwd@users.noreply.github.com>
2023-11-28 06:40:16 +0000

    refactor: aws-lambda-structure

--- BackEnd/api.py
+++ BackEnd/api.py
@@ -2,21 +2,16 @@ from openai import OpenAI
 import json
 client = OpenAI()
 
-json_obj = ""
-def get_response(message):
+def lambda_handler(event, context):
+    message = event["message"]
+    meta = event["meta"]
     response = client.chat.completions.create(
         model="gpt-3.5-turbo-1106",
         response_format={"type":"json_object"},
         temperature=0.7,
-        messages=[{"role":"system", "content":"You are a helpful assistant that extracts data and returns it in JSON format. Need to format as \"input\": qustion, \"result\": answer "},
+        messages=[{"role":"system", "content":"You are a helpful assistant that extracts data and returns it in JSON format. Need to format as \"response\": answer "},
                   {"role":"user","content":message}],
         max_tokens=100
     )
-    return response.choices[0].message.content
-
-message = input()
-response = get_response(message)
-
-json_obj = json.loads(response)
-
-print(json_obj)
+    json_obj = json.loads(response.choices[0].message.content)
+    return json_obj
