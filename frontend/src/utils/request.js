2abbac2e113fefab277efb68429700e60afa8dc9
Stan <lol97554@gmail.com>
2023-12-09 22:37:55 +0800

    chore: change api endpoint.

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 export const fetchMessage = async function (message){
-    const response = await axios.post('/chat', {
+    const response = await axios.post('https://ihr2z62zo5.execute-api.us-east-1.amazonaws.com/prod/chat', {
         "message" : message,
         "meta" : true
     })
