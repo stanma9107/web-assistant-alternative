526660d3c3fe4adfa79fdb87e0d6b3d7035d8040
Stan <lol97554@gmail.com>
2023-12-09 16:53:46 +0800

    fix: change event to AWS Proxy event.

--- BackEnd/api.py
+++ BackEnd/api.py
@@ -4,13 +4,14 @@ import json
 client = OpenAI()
 
 def lambda_handler(event, context):
+    body = json.loads(event['body'])
     response = client.chat.completions.create(
         model="gpt-3.5-turbo-1106",
         response_format={"type":"json_object"},
         temperature=0.7,
         messages=[{"role":"system", "content": "You are a helpful assistant designed to output JSON.\
                         And your json format should only be { \" response \" = your answer }"},
-                {"role":"user","content":event['message']}],
+                {"role":"user","content":body['message']}],
         max_tokens=100
     )
     
