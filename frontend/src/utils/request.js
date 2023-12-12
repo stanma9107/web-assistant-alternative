f581d43fd83b70cfad0fe93182cd0733f1e4ff41
Stan <lol97554@gmail.com>
2023-12-12 14:28:41 +0800

    feat: integrate with ibm function in Lambda.

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -1,10 +1,17 @@
 import axios from 'axios'
 
-
 export const fetchMessage = async function (message){
     const response = await axios.post('https://ihr2z62zo5.execute-api.us-east-1.amazonaws.com/prod/chat', {
         "message" : message,
         "meta" : true
     })
     return response.data.response
-  }
+}
+
+export const fetchIBMMessage = async (message, sessionId) => {
+    const response = await axios.post("https://ihr2z62zo5.execute-api.us-east-1.amazonaws.com/prod/ibm", {
+        "message": message,
+        "session_id": sessionId,
+    })
+    return response.data;
+}
